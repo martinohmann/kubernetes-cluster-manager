e39b0c2f29f8bb7f6bd7a358d66debf92c574a2d
Martin Ohmann <martin.ohmann@bonial.com>
2019-04-07 21:46:03 +0200

    fix travis url, add first tests

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/sirupsen/logrus v1.4.1
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/sys v0.0.0-20190403152447-81d4e9dc473e // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
---
+++ pkg/config/config_test.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApplyDefaults(t *testing.T) {
+	c := &Config{WorkingDir: "/tmp"}
+	c.ApplyDefaults()
+
+	assert.Equal(t, "/tmp/manifest.yaml", c.Manifest)
+	assert.Equal(t, "/tmp/deletions.yaml", c.Deletions)
+	assert.Equal(t, "/tmp/values.yaml", c.Helm.Values)
+	assert.Equal(t, "/tmp/cluster", c.Helm.Chart)
+}
