52e71edc645a03601069d0550ba40d93ccfd6c66
Martin Ohmann <martin.ohmann@bonial.com>
2019-04-09 19:41:21 +0200

    remove unused code

--- pkg/git/filechanges.go
+++ pkg/git/filechanges.go
@@ -1,37 +1,30 @@
 package git
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/martinohmann/kubernetes-cluster-manager/pkg/fs"
 )
 
-// FileChanges is a container for the current content of a file and the changes
-// that should be applied to it.
+// FileChanges is a container for a file and the changes that should be applied
+// to it.
 type FileChanges struct {
 	filename string
-	f        *os.File
-	content  []byte
 	tmpf     *os.File
 	changes  []byte
 }
 
+// NewFileChanges creates a new FileChanges value for given filename and the
+// changes that should be written to it.
 func NewFileChanges(filename string, changes []byte) (*FileChanges, error) {
-	f, err := fs.OpenFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	prefix := filepath.Base(f.Name())
+	prefix := filepath.Base(filename)
 	tmpf, err := fs.NewTempFile(prefix, changes)
 	if err != nil {
 		return nil, err
 	}
 
 	c := &FileChanges{
-		f:        f,
 		tmpf:     tmpf,
 		filename: filename,
 		changes:  changes,
@@ -40,46 +33,24 @@ func NewFileChanges(filename string, changes []byte) (*FileChanges, error) {
 	return c, nil
 }
 
-func (c *FileChanges) Content() []byte {
-	if c.content == nil {
-		content, _ := ioutil.ReadAll(c.f)
-		c.content = content
-	}
-
-	return c.content
-}
-
-func (c *FileChanges) Changes() []byte {
-	return c.changes
-}
-
-func (c *FileChanges) Filename() string {
-	return c.tmpf.Name()
-}
-
+// Apply replaces the file content with the pending changes.
 func (c *FileChanges) Apply() error {
 	if err := fs.WriteFile(c.filename, c.changes); err != nil {
 		return err
 	}
 
-	c.content = c.changes
-
 	return nil
 }
 
+// Close implements io.Closer. Will remove any temporary files that were
+// created during the lifecycle of the value.
 func (c *FileChanges) Close() error {
 	defer os.Remove(c.tmpf.Name())
 
-	err := c.tmpf.Close()
-
-	if err1 := c.f.Close(); err == nil {
-		err = err1
-	}
-
-	return err
+	return c.tmpf.Close()
 }
 
-// GetDiff creates a diff for the file changes and returns it.
+// Diff creates a diff for the file changes and returns it.
 func (c *FileChanges) Diff() (string, error) {
 	return Diff(c.filename, c.tmpf.Name())
 }
